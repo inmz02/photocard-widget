f0175cbe7a7b412a8ed53917ecad588bfde63cc0
izi <inmz02@outlook.com>
2024-05-15 02:06:43 +0100

    Ability to change images!

--- src/script.js
+++ src/script.js
@@ -7,9 +7,13 @@ const closeBtn = document.getElementById("closeBtn");
 const creditsBtn = document.getElementById("creditsBtn");
 const photosBtn = document.getElementById("photosBtn");
 const creditsSection = document.getElementById("creditsSection");
+const photoSetsSection = document.getElementById("photoSetsSection");
+const photoSetCloseBtn = document.getElementById("photoSetCloseBtn");
 const creditsCloseBtns = document.querySelectorAll(".creditsCloseBtn");
 const langBtn = document.getElementById("langBtn");
 let mode = langBtn.dataset.lang;
+const prevButton = document.querySelector(".leftBtn");
+const nextButton = document.querySelector(".rightBtn");
 
 // --------------- //
 
@@ -20,7 +24,10 @@ let mode = langBtn.dataset.lang;
 
 // Hover ON
 cardContainer.addEventListener("mouseenter", function () {
-  if (creditsSection.style.display === "block") {
+  if (
+    creditsSection.style.display === "block" ||
+    photoSetsSection.style.display === "block"
+  ) {
     menuNav.style.display = "none";
     photoControls.style.display = "none";
   } else {
@@ -62,7 +69,19 @@ creditsCloseBtns.forEach(function (x) {
   });
 });
 
+photoSetCloseBtn.addEventListener("click", function () {
+  photoSetsSection.style.display = "none";
+  menuNav.style.display = "flex";
+  closedMenu.style.display = "flex";
+  photoControls.style.display = "flex";
+});
 
+// Photcard Set
+photosBtn.addEventListener("click", function () {
+  openedMenu.style.display = "none";
+  photoControls.style.display = "none";
+  photoSetsSection.style.display = "block";
+});
 
 langBtn.addEventListener("click", function () {
   let p = langBtn.querySelector("p");
@@ -72,17 +91,49 @@ langBtn.addEventListener("click", function () {
     langBtn.dataset.lang = "en";
     creditsSection.querySelector("#koreanCredits").style.display = "none";
     creditsSection.querySelector("#engCredits").style.display = "block";
+    photoSetsSection.querySelector("#korH1").style.display = "none";
+    photoSetsSection.querySelector("#engH1").style.display = "block";
     mode = "EN";
   } else {
     p.textContent = "KR";
     langBtn.dataset.lang = "kr";
     creditsSection.querySelector("#koreanCredits").style.display = "block";
     creditsSection.querySelector("#engCredits").style.display = "none";
+    photoSetsSection.querySelector("#korH1").style.display = "block";
+    photoSetsSection.querySelector("#engH1").style.display = "none";
     mode = "kr";
   }
 });
 
-
 setTimeout(() => {
   mode = langBtn.dataset.lang;
 }, 1000);
+
+// 🖼️ Image sorting!
+let folderNum = "1";
+let numbOfPics = 10;
+let currentImageIndex = 1;
+
+prevButton.addEventListener("click", () => {
+  currentImageIndex--;
+  if (currentImageIndex < 1) {
+    currentImageIndex = numbOfPics;
+  }
+  changeBackgroundImage();
+});
+
+nextButton.addEventListener("click", () => {
+  currentImageIndex++;
+  if (currentImageIndex > numbOfPics) {
+    currentImageIndex = 1;
+  }
+  changeBackgroundImage();
+});
+
+function changeBackgroundImage() {
+  cardContainer.style.backgroundImage = `url('src/images/${folderNum}/${currentImageIndex}.png')`;
+}
+
+console.log(`url('images/${folderNum}/${currentImageIndex}.png')`);
+
+changeBackgroundImage();
