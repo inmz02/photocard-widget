d024402f1a39bd1341d4c01dd7a1396ad7436dd0
izi <inmz02@outlook.com>
2024-05-17 11:29:31 +0100

    Created the slideshow container

--- src/install.js
+++ src/install.js
@@ -1,15 +1,21 @@
 // >> 🏠 DOM elements
 const musicBox = document.querySelector(".musicBox");
 const audio = document.querySelector("audio");
+const containsTitles = document.querySelectorAll(
+  ".moreInfoSections .contain .title"
+);
+const rowOfImages = document.querySelectorAll(".rowofimages div img");
+const mainimage = document.querySelector(".mainimage img");
 
 // >> 💡 Initializations
 let isPlaying = false;
-audio.loop = true;
 audio.volume = 0.3;
 
-
 // >> 🧁 Functions
-
+function loopAudio() {
+  audio.currentTime = 0;
+  audio.play();
+}
 
 // >> 🐼 Event Listeners
 musicBox.addEventListener("click", () => {
@@ -22,4 +28,39 @@ musicBox.addEventListener("click", () => {
     isPlaying = true;
     musicBox.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
   }
+});
+
+audio.addEventListener("ended", loopAudio);
+
+containsTitles.forEach(function (title) {
+  title.addEventListener("click", () => {
+    let parentElement = title.parentNode;
+    let middleCon = parentElement.querySelector(".middle");
+    middleCon.classList.toggle("hide");
+
+    // Update the icon class based on the state of middleCon
+    let icon = title.querySelector("i");
+    if (middleCon.classList.contains("hide")) {
+      icon.classList.remove("fa-caret-down");
+      icon.classList.add("fa-caret-up");
+    } else {
+      icon.classList.remove("fa-caret-up");
+      icon.classList.add("fa-caret-down");
+    }
+  });
+});
+
+rowOfImages.forEach(function (selectedImage) {
+  selectedImage.addEventListener("click", () => {
+    rowOfImages.forEach(function (image) {
+      let parentElement = image.parentNode;
+      parentElement.classList.remove("selected");
+    });
+
+    let imageURL = selectedImage.getAttribute("src");
+    let parentElement = selectedImage.parentNode;
+
+    parentElement.classList.add("selected");
+    mainimage.setAttribute("src", imageURL);
+  });
 });
